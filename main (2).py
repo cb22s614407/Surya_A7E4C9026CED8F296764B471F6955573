2550c73a7edb64236ec4194c5f73305d34412ce5
cb22s614407 <145005607+cb22s614407@users.noreply.github.com>
2023-09-14 15:03:00 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,22 @@
+class chargcard:
+
+  def getbalance(self):
+    return
+    self._balance
+
+
+def getlimit(self):
+  return
+  self._limit
+
+
+def charge(self, amount):
+  if self._balance + amount <= self._self._limit:
+    self._balance += amount
+    return True
+  else:
+    return False
+
+
+def deposit(self, amount):
+  self._balancr -= amount
